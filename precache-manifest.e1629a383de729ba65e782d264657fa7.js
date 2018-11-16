2900cdc777f20e70f54b2c8d58b863dc6722ad23
sola <solaocodes@gmail.com>
2018-11-16 18:11:06 -0500

    Updates

---
+++ precache-manifest.e1629a383de729ba65e782d264657fa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a46a8c5d1b014085ebf5",
+    "url": "/tic-tac-toe/static/css/main.049d15e3.chunk.css"
+  },
+  {
+    "revision": "a46a8c5d1b014085ebf5",
+    "url": "/tic-tac-toe/static/js/main.a46a8c5d.chunk.js"
+  },
+  {
+    "revision": "07861e9d5e216d6b0c3e",
+    "url": "/tic-tac-toe/static/js/1.07861e9d.chunk.js"
+  },
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "a66a238841d9b19f9f7ec99c27d33c56",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
